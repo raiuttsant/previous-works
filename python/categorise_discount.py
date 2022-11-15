3ed341dd8f6d69c710109f289776885ba11e3469
Uttsant Rai <97908036+raiuttsant@users.noreply.github.com>
2022-11-15 20:47:30 +0800

    Update and rename fleet_benefit_weekly.py to python/categorise_discount.py
    The script uses a special package to ensure column name consistency to allow the merge of two inputted CSV files.
    This way, even if there are minor inconsistencies between two key fields e.g. a small typo/capitalisation, the abnormal field will still be matched with the optimal one.
    For instance, "Water bottle" & "water bottle".
    After this normalisation, the orders are categorised into 3 types according to their product type field.
    Finally, the weekly total discount amount is evaluated for the 3 categories.

--- python/categorise_discount.py
+++ python/categorise_discount.py
@@ -1,33 +1,35 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Jul  7 14:27:27 2022
-
-@author: uttsant
-"""
-
 
 import pandas as pd
 import numpy as np
 from fuzzywuzzy import process
 
-product_path = r'/Users/uttsant/Desktop/foodpanda_coding/products_export_1.csv'
-discount_path = r'/Users/uttsant/Desktop/foodpanda_coding/fleet_benefit_weekly.csv'
+# file path for product list and order + discount info
+product_path = r'/insert path here/products_export_1.csv'
+discount_path = r'/insert path here/fleet_benefit_weekly.csv'
 
 df_product = pd.read_csv(product_path)
 df_discount = pd.read_csv(discount_path)
 
+# keep necessary columns
 df_product = df_product[['Handle', 'Title','Custom Product Type', 'Tags', 'Variant Price',
          'Variant Compare At Price','Cost per item', 'Status']]
 
+# drop rows where "Custom Product Type" field is empty
 df_product.dropna(axis=0, subset=['Custom Product Type'], inplace=True)
 
-# get the sum of all the entries with an empty product title
+# get the sum of all the entries with an empty product title --> this line of code validates the need for the next line
 df_discount[df_discount['product_title'].isnull() == True].sum()
 
+# drop rows where "product_title" field is empty
 df_discount.dropna(axis=0, subset = ['product_title'], inplace=True)
 
+# create a list containing unique product titles
 product_list = df_product['Title'].unique()
+
+# create a function which using the fuzzywuzzy package to return the best match for the product titles in the order info file, 
+# as product titles in the product list file and order info file may not be consistent, to facilitate the merge later
 
 def match_title(product_title):
     try:
@@ -36,23 +38,21 @@ def match_title(product_title):
     except:
         return(np.nan)
 
-#create new column in discount dataframe using product titles from product dataframe
+# create new column in the order dataframe using the product titles generates from the function above
 df_discount['official_title'] = df_discount['product_title'].apply(match_title)
 
-#merge the two dataframes
+# merge the two dataframes
 final_df = pd.merge(df_discount, df_product, how='left', left_on=['official_title'], right_on=['Title'])
 
+# categorise the consolidated order info into 3 different series i.e. Merchandise (MER), Mandatory Equipment (MAN), Equipment (EQP)
+# the 3 series contain the weekly total discount amount for their respective product categories
 mer_series = final_df[final_df['Custom Product Type'].str.contains('MER')].groupby(['week'])['total_discount_amount'].sum()
 man_series = final_df[final_df['Custom Product Type'].str.contains('MAN')].groupby(['week'])['total_discount_amount'].sum()
 eqp_series = final_df[final_df['Custom Product Type'].str.contains('EQP')].groupby(['week'])['total_discount_amount'].sum()
+
+# concatenate the 3 series vertically and export it as a csv
 data = {"MERCHANDISE": mer_series,
         "MANDATORY EQUIPMENT": man_series,
         "EQUIPMENT": eqp_series}
 export_df = pd.concat(data, axis = 1)
-export_df.to_csv('/Users/uttsant/Desktop/foodpanda_coding/weekly_fleet_export.csv')
-
-print(f"MERCHANDISE\n{final_df[final_df['Custom Product Type'].str.contains('MER')].groupby(['week'])['total_discount_amount'].sum()}")
-print(f"MANDATORY EQUIPMENT\n{final_df[final_df['Custom Product Type'].str.contains('MAN')].groupby(['week'])['total_discount_amount'].sum()}")
-print(f"EQUIPMENT\n{final_df[final_df['Custom Product Type'].str.contains('EQP')].groupby(['week'])['total_discount_amount'].sum()}")
-print(f"TOTAL\n{final_df.groupby('week')['total_discount_amount'].sum()}")
-print(f"BLACK THERMAL\n{final_df[final_df['product_title'] == 'Limited Edition Black Thermal Bags'].groupby('week')['total_discount_amount'].sum()}")
+export_df.to_csv('/insert path here/weekly_fleet_export.csv')
