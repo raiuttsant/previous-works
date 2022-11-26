22959b161b90b7ef4f3247fdb900a1d69712c3bf
Uttsant Rai <97908036+raiuttsant@users.noreply.github.com>
2022-11-26 13:11:31 +0800

    Update vs_other_countries.sql

--- bigquery_sql/vs_other_countries.sql
+++ bigquery_sql/vs_other_countries.sql
@@ -1,20 +1,25 @@
 SELECT 
-  DATE(rd.rider_accepted_at_local) AS day,
+ --  monthly basis
+  EXTRACT(YEAR FROM CAST(created_date_utc AS DATETIME)) AS year,
+  EXTRACT(MONTH FROM CAST(created_date_utc AS DATETIME)) AS month,
   global_entity_id AS region,
+ --   group by different verticals
   CASE WHEN rider.vendor.vertical_type IN ('darkstores','restaurants') 
     THEN rider.vendor.vertical_type
     WHEN rider.vendor.vertical_type IN ('courier_business')  
     THEN 'pandago'
-  WHEN rider.vendor.vertical_type IN ('caterers')  
+    WHEN rider.vendor.vertical_type IN ('caterers')  
     THEN 'caterers'
     ELSE 'shops' END AS vertical_type,
-    COUNT(DISTINCT lgo.order_code) AS order_number
+ --   aggregate order numbers
+  COUNT(DISTINCT lgo.order_code) AS order_number
 FROM `fulfillment-dwh-production.pandata_curated.lg_orders` lgo
 LEFT JOIN UNNEST(rider.deliveries) rd
-WHERE created_date_utc >= '2022-10-01'
-    AND DATE(rd.rider_accepted_at_local) >= '2022-10-01'
+WHERE created_date_utc >= '2022-01-01'
+    AND DATE(rd.rider_accepted_at_local) >= '2022-01-01'
+--  list of countries
     AND global_entity_id IN ('FP_HK', 'FP_SG', 'FP_TW', 'FP_MY', 'FP_PH','FP_TH', 'FP_PK')
     AND rider.order_status = 'completed'
     AND delivery_type = 'OWN_DELIVERY'
-GROUP BY 1,2,3
-ORDER BY day, region, order_number DESC
+GROUP BY 1,2,3,4
+ORDER BY year, month, region, order_number DESC
